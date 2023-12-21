a5526cc0b691c12593e780eed03ab0a4f609ee4a
opilane <opilane@HP-CZC0359NN3>
2023-12-21 14:31:48 +0200

    Add project files.

---
+++ Pyramid Code/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Pyramid_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int numberoflayer = 6, Space, Number;
+            Console.WriteLine("Print paramid");
+            for (int i = 1; i <= numberoflayer; i++) // Total number of layer for pramid  
+            {
+                for (Space = 1; Space <= (numberoflayer - i); Space++) // Loop For Space  
+                    Console.Write(" ");
+                for (Number = 1; Number <= i; Number++) //increase the value  
+                    Console.Write(Number);
+                for (Number = (i - 1); Number >= 1; Number--) //decrease the value  
+                    Console.Write(Number);
+                Console.WriteLine();
+            }
+        }
+    }
+}
